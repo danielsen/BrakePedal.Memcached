979f60871ada0f5492b89d091f96887db3ab7a8c
danielsen <dnielsen@reachmail.com>
2018-05-04 16:06:09 -0500

    Adds memcached throttle repository.

---
+++ BrakePedal.Memcached.Tests/MemcachedThrottleRepositoryTests.cs
@@ -0,0 +1,100 @@
+﻿using NUnit.Framework;
+using Should;
+using Enyim.Caching;
+using NSubstitute;
+
+namespace BrakePedal.Memcached.Tests
+{
+    [TestFixture]
+    public class MemcachedThrottleRepositoryTests
+    {
+        private IMemcachedClient _memcachedClient;
+        private MemcachedThrottleRepository _memcachedThrottleRepository;
+        private SimpleThrottleKey _simpleThrottleKey;
+
+        [SetUp]
+        public void Setup()
+        {
+            _memcachedClient = Substitute.For<IMemcachedClient>();
+            _memcachedThrottleRepository = new MemcachedThrottleRepository(_memcachedClient);
+            _simpleThrottleKey = new SimpleThrottleKey("test", "key");
+        }
+
+        [Test]
+        public void should_correctly_increment_key()
+        {
+            Limiter limiter = new Limiter().Limit(1).Over(10);
+            var id = _memcachedThrottleRepository.CreateThrottleKey(_simpleThrottleKey, limiter);
+
+            _memcachedClient.Increment(id, 1, 1, limiter.Period).Returns((ulong) 1);
+
+            _memcachedThrottleRepository.AddOrIncrementWithExpiration(_simpleThrottleKey, limiter);
+
+            _memcachedClient.Received(1).Increment(id, 1, 1, limiter.Period);
+            _memcachedClient.Received(1).Increment(id, 1, 0, limiter.Period);
+        }
+
+        [Test]
+        public void should_return_null_for_nonexistent_key()
+        {
+            Limiter limiter = new Limiter().Limit(1).Over(10);
+            var id = _memcachedThrottleRepository.CreateThrottleKey(_simpleThrottleKey, limiter);
+
+            _memcachedClient.Get<string>(id).Returns("foo");
+
+            long? result = _memcachedThrottleRepository.GetThrottleCount(_simpleThrottleKey, limiter);
+
+            result.ShouldEqual(null);
+        }
+
+        [Test]
+        public void should_return_value_for_key()
+        {
+            Limiter limiter = new Limiter().Limit(1).Over(10);
+            var id = _memcachedThrottleRepository.CreateThrottleKey(_simpleThrottleKey, limiter);
+
+            _memcachedClient.Get<string>(id).Returns("10");
+
+            long? result = _memcachedThrottleRepository.GetThrottleCount(_simpleThrottleKey, limiter);
+
+            result.ShouldEqual(10);
+        }
+
+        [Test]
+        [TestCase(true, true)]
+        [TestCase(false, false)]
+        public void should_correctly_identify_lock_status(bool keyExists, bool expected)
+        {
+            Limiter limiter = new Limiter().Limit(1).Over(10).LockFor(1);
+            var id = _memcachedThrottleRepository.CreateThrottleKey(_simpleThrottleKey, limiter);
+
+            _memcachedClient.TryGet(id, out object target).ReturnsForAnyArgs(keyExists);
+
+            bool result = _memcachedThrottleRepository.LockExists(_simpleThrottleKey, limiter);
+
+            result.ShouldEqual(expected);
+        }
+
+        [Test]
+        public void should_remove_throttle()
+        {
+            Limiter limiter = new Limiter().Limit(1).Over(10).LockFor(1);
+            var id = _memcachedThrottleRepository.CreateThrottleKey(_simpleThrottleKey, limiter);
+            
+            _memcachedThrottleRepository.RemoveThrottle(_simpleThrottleKey, limiter);
+
+            _memcachedClient.Received(1).Remove(id);
+        }
+
+        [Test]
+        public void should_set_lock()
+        {
+            Limiter limiter = new Limiter().Limit(1).Over(10).LockFor(1);
+            var id = _memcachedThrottleRepository.CreateLockKey(_simpleThrottleKey, limiter);
+            
+            _memcachedThrottleRepository.SetLock(_simpleThrottleKey, limiter);
+
+            _memcachedClient.Received(1).Increment(id, 1, 1, limiter.LockDuration.Value);
+        }
+    }
+}
---
+++ BrakePedal.Memcached/MemcachedThrottleRepository.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Enyim.Caching;
+
+namespace BrakePedal.Memcached
+{
+    public class MemcachedThrottleRepository : IThrottleRepository
+    {
+        private readonly IMemcachedClient _memcachedClient;
+
+        public object[] PolicyIdentityValues { get; set; }
+
+        public MemcachedThrottleRepository(IMemcachedClient memcachedClient)
+        {
+            _memcachedClient = memcachedClient;
+        }
+
+        public long? GetThrottleCount(IThrottleKey key, Limiter limiter)
+        {
+            string id = CreateThrottleKey(key, limiter);
+            var value = _memcachedClient.Get<string>(id);
+
+            if (long.TryParse(value, out long target))
+                return target;
+            return null;
+        }
+
+        public void AddOrIncrementWithExpiration(IThrottleKey key, Limiter limiter)
+        {
+            string id = CreateThrottleKey(key, limiter);
+            ulong result = _memcachedClient.Increment(id, 1, 1, limiter.Period);
+
+            if (result == 1)
+                _memcachedClient.Increment(id, 1, 0, limiter.Period);
+        }
+
+        public bool LockExists(IThrottleKey key, Limiter limiter)
+        {
+            string id = CreateLockKey(key, limiter);
+            return _memcachedClient.TryGet(id, out _);
+        }
+
+        public void SetLock(IThrottleKey key, Limiter limiter)
+        {
+            string id = CreateLockKey(key, limiter);
+            _memcachedClient.Increment(id, 1, 1, limiter.LockDuration.Value);
+        }
+
+        public void RemoveThrottle(IThrottleKey key, Limiter limiter)
+        {
+            string id = CreateThrottleKey(key, limiter);
+            _memcachedClient.Remove(id);
+        }
+
+        public string CreateThrottleKey(IThrottleKey key, Limiter limiter)
+        {
+            List<object> values = CreateBaseKeyValues(key, limiter);
+
+            string countKey = TimeSpanToFriendlyString(limiter.Period);
+            values.Add(countKey);
+
+            if (limiter.Period.TotalSeconds == 1)
+                values.Add(GetUnixTimestamp());
+
+            string id = string.Join(":", values);
+            return id;
+        }
+
+        public string CreateLockKey(IThrottleKey key, Limiter limiter)
+        {
+            List<object> values = CreateBaseKeyValues(key, limiter);
+
+            string lockKeySuffix = TimeSpanToFriendlyString(limiter.LockDuration.Value);
+            values.Add("lock");
+            values.Add(lockKeySuffix);
+
+            string id = string.Join(":", values);
+            return id;
+        }
+
+        private string TimeSpanToFriendlyString(TimeSpan span)
+        {
+            var items = new List<string>();
+            Action<double, string> ifNotZeroAppend = (value, key) =>
+            {
+                if (value != 0)
+                    items.Add(string.Concat(value, key));
+            };
+
+            ifNotZeroAppend(span.Days, "d");
+            ifNotZeroAppend(span.Hours, "h");
+            ifNotZeroAppend(span.Minutes, "m");
+            ifNotZeroAppend(span.Seconds, "s");
+
+            return string.Join("", items);
+        }
+        
+        private List<object> CreateBaseKeyValues(IThrottleKey key, Limiter limiter)
+        {
+            List<object> values = key.Values.ToList();
+            if (PolicyIdentityValues != null && PolicyIdentityValues.Length > 0)
+                values.InsertRange(0, PolicyIdentityValues);
+
+            return values;
+        }
+
+        private long GetUnixTimestamp()
+        {
+            TimeSpan timeSpan = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0));
+            return (long)timeSpan.TotalSeconds;
+        }
+    }
+}
